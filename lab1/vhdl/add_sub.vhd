78d63f2cd71858279f26edf6b3115b986c0865ae
Victor B. Gergaud <61604940+vgergaud@users.noreply.github.com>
2020-09-25 15:18:51 +0200

    Update add_sub

--- lab1/vhdl/add_sub.vhd
+++ lab1/vhdl/add_sub.vhd
@@ -14,5 +14,19 @@ entity add_sub is
 end add_sub;
 
 architecture synth of add_sub is
+    
+    signal s_xor_output, s_adder_output : signed(31 downto 0); 
+
 begin
+
+    s_xor_output <= b xor sub_mode;
+
+    s_adder_output <= signed(a) + s_xor_output + to_signed(sub_mode, 32);
+
+    zero <= '1' when s_adder_output = to_signed(0, 32) else '0';
+
+    carry <= s_adder_output(0); 
+
+    r <= std_logic_vector(s_adder_output);
+
 end synth;
