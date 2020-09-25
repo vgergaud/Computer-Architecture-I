bfb54b92674b4b416196bf0170e45fd897b67ffd
Victor B. Gergaud <61604940+vgergaud@users.noreply.github.com>
2020-09-25 15:20:43 +0200

    Update multiplexer

--- lab1/vhdl/multiplexer.vhd
+++ lab1/vhdl/multiplexer.vhd
@@ -14,4 +14,8 @@ end multiplexer;
 
 architecture synth of multiplexer is
 begin
+    o <= i0 when sel = "00" else
+        i1 when sel = "01" else
+        i2 when sel = "10" else
+        i3 when sel = "11";
 end synth;
